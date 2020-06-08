28d315b13f885bc65baf98243964b46f62e54a2c
Hari Sapna Nair <56690856+Sapna2001@users.noreply.github.com>
2020-06-08 15:08:22 +0530

    calculator.py updated

--- Calculator.py
+++ Calculator.py
@@ -1,70 +1,54 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[6]:
-
-
-#Calculator
-#Bellatrix Mini Project 1
-class Add():
-    def add(self):
+#functions
+def add():
         a=int(input('Enter the 1st number'))
         b=int(input('Enter the 2nd number'))
         c=a+b
-        print("Addition of {} and {} is : ".format(a,b),c)
-class Sub():
-    def sub(self):
+        print("Addition of {} and {} is : ".format(a,b),c)  
+
+def sub():
         a=int(input('Enter the 1st number'))
         b=int(input('Enter the 2nd number'))
         c=a-b
         print("Subtraction of {} and {} is : ".format(a,b),c)
-class Mul():
-    def mul(self):
+
+def mul():
         a=int(input('Enter the 1st number'))
         b=int(input('Enter the 2nd number'))
         c=a*b
         print("Multiplication of {} and {} is : ".format(a,b),c)
-class Mod():
-    def mod(self):
-        a=int(input('Enter the 1st number'))
-        b=int(input('Enter the 2nd number'))
-        c=a%b
-        print("Modulation of {} and {} is : ".format(a,b),c)
-class Per():
-    def per(self):
-        a=int(input('Enter the number that you have scored'))
-        b=int(input('Enter the Total numbers'))
-        c=(a/b)*100
-        print("Persentage of that you have scored is : ".format(a,b),c)
-class Div(Add,Sub,Mul,Mod,Per):
-    def div(self):
+
+def div():
         a=int(input('Enter the 1st number'))
         b=int(input('Enter the 2nd number'))
         c=a/b
-        print("Division of {} and {} is : ".format(a,b),c)
-a=Div()
-a.add()
-a.sub()
-a.mul()
-a.div()
-a.mod()
-a.per()
+        print("Division of {} and {} is : ".format(a,b),c)    
 
-
-# In[ ]:
-
-
-
-
+#main function         
+print("This Is A Calculator")
+while True:
+  #options
+  print("The Following Task Can Be Done:")
+  print("--------------------------------")
+  print("Press 1 For Addition")
+  print("Press 2 For Substraction")
+  print("Press 3 For Muliplication")
+  print("Press 4 For Divison")
+  print("Press 5 For Calculating Trignometric Ratioes")
+  print("Press 6 To Quit")
+  print("---------------------------------------------")
+  choice=int(input("Please Choose Any"))
+  #choice
+  if choice == 'Q':
+    break;
+  elif choice==1:
+    add()
+  elif choice==2:
+    sub()
+  elif choice==3:
+    mul()
+  elif choice==4:
+    div()
+  elif choice==5:
+    trigonometry()  
+  else:
+    print("Invalid Option")
